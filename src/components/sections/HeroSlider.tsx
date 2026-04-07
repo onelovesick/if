40ec9dd1f16b2eb3be9cb327064a602fa14c9dc3
onelovesick <yousifjabak74@gmail.com>
2026-04-06 21:02:49 -0400

    Update HeroSlider.tsx

--- src/components/sections/HeroSlider.tsx
+++ src/components/sections/HeroSlider.tsx
@@ -35,12 +35,10 @@ const heroHtml = `<style>
   position: absolute;
   top: 50%;
   left: 50%;
-  min-width: 100%;
-  min-height: 100%;
-  width: auto;
-  height: auto;
+  width: 100%;
+  height: 100%;
   transform: translate(-50%, -50%);
-  object-fit: cover;
+  object-fit: contain;
 }
 
 /* Multi-layer video blending */
