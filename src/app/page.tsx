e8bf8af0bd917b262c41f1bf7220947113f40f1b
onelovesick <yousifjabak74@gmail.com>
2026-04-09 17:48:47 -0400

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import HeroSlider from '@/components/sections/HeroSlider'
-import HeroIntro from '@/components/sections/HeroIntro'
 import SectionStatement from '@/components/sections/SectionStatement'
 import Section1   from '@/components/sections/Section1'
 import Section2   from '@/components/sections/Section2'
@@ -10,33 +9,24 @@ import Section6   from '@/components/sections/Section6'
 import Section7   from '@/components/sections/Section7'
 import Section8   from '@/components/sections/Section8'
 import Section10  from '@/components/sections/Section10'
+import PageLoader  from '@/components/layout/PageLoader'
 
 export default function HomePage() {
   return (
     <>
+      <PageLoader />
       <main style={{ margin: 0, padding: 0 }}>
-        <HeroIntro />
         <HeroSlider />
-        {/* Content scrolls OVER the fixed hero */}
-        <div style={{
-          position: 'relative',
-          zIndex: 10,
-          background: '#F4F7FA',
-          borderRadius: '20px 20px 0 0',
-          boxShadow: '0 -20px 60px rgba(0,0,0,0.3)',
-          overflow: 'hidden',
-        }}>
-          <SectionStatement />
-          <Section1 />
-          <Section2 />
-          <Section3 />
-          <Section4 />
-          <Section5 />
-          <Section6 />
-          <Section10 />
-          <Section8 />
-          <Section7 />
-        </div>
+        <SectionStatement />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
+        <Section10 />
+        <Section8 />
+        <Section7 />
       </main>
     </>
   )
